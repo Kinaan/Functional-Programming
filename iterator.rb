d3af152b264cbb132a94cc0b350f04273f22ff96
Kinaan Khan Sherwani <mr.sherwani@gmail.com>
2016-12-25 16:43:13 +0500

    implementation of filter

--- iterator.rb
+++ iterator.rb
@@ -8,4 +8,13 @@ module Iteratorr
     end
     mapped
   end
+
+  def self.filter obj
+  	filtered = []
+  	for i in 0..obj.length - 1
+      result = yield(obj[i])
+      filtered << obj[i] if result
+    end
+    filtered
+  end
 end

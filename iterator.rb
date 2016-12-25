614af8b18dc257b0ccf052b56ff93f7587fd5763
Kinaan Khan Sherwani <mr.sherwani@gmail.com>
2016-12-25 18:01:41 +0500

    implementation of reduce, corrected module name, fixed indentation

--- iterator.rb
+++ iterator.rb
@@ -1,6 +1,6 @@
 #!/bin/ruby
 
-module Iteratorr
+module Iterator
   def self.map obj
     mapped = []
     for i in 0..obj.length - 1
@@ -10,11 +10,19 @@ module Iteratorr
   end
 
   def self.filter obj
-  	filtered = []
-  	for i in 0..obj.length - 1
+    filtered = []
+    for i in 0..obj.length - 1
       result = yield(obj[i])
       filtered << obj[i] if result
     end
     filtered
+  end
+
+  def self.reduce obj
+    reduced_result = obj.first
+    for i in 1..obj.length - 1
+      reduced_result = yield reduced_result, obj[i]
+    end
+    reduced_result
   end
 end
